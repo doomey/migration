a58b2acd6e3383f05462417762b0eb06e14fd3b2
HAEBEOM <haebeompark@naver.com>
2016-03-16 14:34:35 +0900

    s3 key, secret, bucket 수정

--- config/s3Config.js
+++ config/s3Config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    "key" : process.env.GREEN_S3_KEY,
-    "secret" : process.env.GREEN_S3_SECRET,
+    "key" : process.env.GREEN_S3_KEY2,
+    "secret" : process.env.GREEN_S3_SECRET2,
     "region" : "ap-northeast-2", //-1은 도쿄, -2는 서울을 의미한다.
-    "bucket" : "greenhero",
+    "bucket" : "greenhero2",
     //아래 image--- 키 이름은 임의로 정해도 된다.
     "imageDir" : "photos",
     "bgDir" : "bg",
